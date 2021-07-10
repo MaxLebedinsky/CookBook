4b73815c51cf78fb4826868403e20609272c7296
dvsolodov <d.v.solodov@yandex.ru>
2021-07-11 01:28:54 +1000

    Created api routes for dishes

--- routes/api/dishes.php
+++ routes/api/dishes.php
@@ -1,1 +1,9 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::post('/', 'DishController@store')->name('dishes.store');
+Route::get('/', 'DishController@index')->name('dishes.index');
+Route::get('/{id}', 'DishController@show')->name('dishes.show');
+Route::match(['put', 'patch'], '/{id}', 'DishController@update')->name('dishes.update');
+Route::delete('/{id}', 'DishController@delete')->name('dishes.delete');
