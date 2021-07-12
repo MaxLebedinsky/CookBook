4444307dd344fff9199a7db06d92243fb62348fc
dvsolodov <d.v.solodov@yandex.ru>
2021-07-12 13:05:29 +1000

    Added routes for inredients

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,9 @@ Route::group(["prefix" => "{$api_version}"], function () {
     // register categories routes
     Route::prefix('categories')
         ->group(base_path('routes/api/categories.php'));
+    // register ingredients routes
+    Route::prefix('dishes/{dish_id}/ingredients')
+        ->group(base_path('routes/api/ingredients.php'));
     // register dishes routes
     Route::prefix('dishes')
         ->group(base_path('routes/api/dishes.php'));
