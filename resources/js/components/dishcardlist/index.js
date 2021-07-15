409aabf3a1c9840b7fa7d4b96b6b2239ed8eba14
dkimd5 <dkimd5@mail.ru>
2021-07-15 21:23:36 +0300

    fetch created

--- resources/js/components/dishcardlist/index.js
+++ resources/js/components/dishcardlist/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./styles.css"
 import { DishCard } from './dishcard';
 
+// const API_URL = 'http://localhost:8000';
+
 export const DishCardList = () => {
+
+   useEffect(() => {
+      fetch({
+         url: 'http://localhost:8000/api/v1/dishes',
+         headers: {
+            'Access-Control-Allow-Origin': '*',
+         }
+      })
+         .then(response => {
+            console.log(response);
+            return response.json();
+         })
+         .then(data => console.log(data))
+   }, [])
+
    return (
       <ul className="list">
          <li className="list-item">
