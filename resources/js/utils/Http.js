2faca28ef632d7589076eb7d1970f2a66be2a350
Marina <maryaleks@mail.ru>
2021-07-19 15:23:28 +0300

    add filter to the searchfield

--- resources/js/utils/Http.js
+++ resources/js/utils/Http.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-console */
 import axios from 'axios'
 
-const API_URL = (process.env.NODE_ENV === 'test') ? process.env.BASE_URL || (`http://localhost:${process.env.PORT}/`) : `/`;
+const API_URL = (process.env.NODE_ENV === 'test') ? process.env.BASE_URL || (`http://localhost:${process.env.PORT}/`) : `http://localhost:8000/`;
 
 axios.defaults.baseURL = API_URL;
 axios.defaults.headers.common.Accept = 'application/json';
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-
+console.log(API_URL)
 export default axios
