429b3eabfc91bd30c7ede4dd679591e053df4de7
dkimd5 <dkimd5@mail.ru>
2021-07-21 08:52:34 +0300

    mergin main changes

--- resources/js/components/dishcardlist/index.js
+++ resources/js/components/dishcardlist/index.js
@@ -2,16 +2,16 @@ import React, { useEffect } from 'react';
 import "./styles.css"
 import { DishCard } from './dishcard';
 
-// const API_URL = 'http://localhost:8000';
+const API_URL = 'http://localhost:8000/api/v1/dishes';
 
 export const DishCardList = () => {
 
    useEffect(() => {
-      fetch({
-         url: 'http://localhost:8000/api/v1/dishes',
+      fetch(API_URL, {
+         type: 'GET',
          headers: {
             'Access-Control-Allow-Origin': '*',
-         }
+         },
       })
          .then(response => {
             console.log(response);
