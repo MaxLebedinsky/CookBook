8413159928694cf5012d7b23a9bf452587a4c6d7
dvsolodov <d.v.solodov@yandex.ru>
2021-07-22 11:12:19 +1000

    Created a migration to add complexity column into dishes table

---
+++ database/migrations/2021_07_22_005553_add_complexity_column_into_dishes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddComplexityColumnIntoDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('dishes', function (Blueprint $table) {
+            $table->tinyInteger('complexity')->unsigned()->after('rating');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('dishes', function (Blueprint $table) {
+            $table->dropColumn('complexity');
+        });
+    }
+}
