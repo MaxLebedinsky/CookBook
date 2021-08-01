eb907675209e940bf7b1fd38d121808e3dd9c66c
Max <fix99@mail.ru>
2021-08-01 19:20:14 +0300

    Added const TEST_DISH_FOR_POST

--- resources/js/components/adddishform/index.js
+++ resources/js/components/adddishform/index.js
@@ -1,9 +1,19 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
-// import { TEST_FULL_DISH } from '../dish/const';
+import { TEST_DISH_FOR_POST } from '../dish/const';
 
-const postDish = () => {
-    console.log('click!');
+const postDish = async () => {
+    console.log(JSON.stringify(TEST_DISH_FOR_POST));
+    let response = await fetch('/api/v1/full-dishes/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(TEST_DISH_FOR_POST)
+      });
+      
+      let result = await response.json();
+      console.log(result.message);
 };
 
 export const AddDishForm = () => {
