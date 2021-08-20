2ee22ee38b6af18ced7dc545fc1e5da0d024472e
Max <fix99@mail.ru>
2021-08-20 20:31:22 +0300

    Converted styles.css for DishCardList into styled.js

--- resources/js/components/dishcardlist/styled.js
+++ resources/js/components/dishcardlist/styled.js
@@ -2,16 +2,26 @@ import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles((theme) => ({
    list:{
-      padding: '5px',
+      // padding: '5px',
       listStyle: 'none'
    },
    listItem:{
-      marginBottom: '10px',
-      flexGrow: '1',
+      margin: '0 auto 10px',
+      // flexGrow: '1',
    },
    showMoreButton:{
       backgroundColor:'#3F51B5',
       color:'white'
+   },
+   [theme.breakpoints.down(374)]: {
+      listItem:{
+         maxWidth: 300,
+      }
+   },
+   [theme.breakpoints.up(425)]: {
+      listItem:{
+         maxWidth: 400,
+      }
    },
    [theme.breakpoints.up(768)]: {
       list:{
